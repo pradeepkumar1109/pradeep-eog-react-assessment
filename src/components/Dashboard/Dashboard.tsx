20dd2b410088db6e261a479ded7e1c904e973a46
pradeepkumar1109 <pradeepkumarp1109@gmail.com>
2020-11-19 14:54:29 -0600

    Bug: Fix an error on intial render not showing current values

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,36 +1,27 @@
 import React from 'react';
 import { Card, CardContent } from '@material-ui/core';
-import { gql, useSubscription } from '@apollo/client';
+import { gql } from '@apollo/client';
 import { client } from '../../App';
 import Header from '../Header';
 import DashboardHeader from './DashboardHeader';
+import { useSubscription } from '@apollo/react-hooks';
 import Chart from './Chart';
 
-export interface Measurement {
-  metric: string;
-  at: number;
-  value: number;
-  unit: string;
-}
+//grab date object for thirty minutes prior to be passed into query
+const thirtyMinutesAgo = new Date(Date.now() - 30 * 60000).getTime();
 
-interface MeasurementSubscription {
-  newMeasurement: Measurement;
-}
+//metric names are fetched from the API and passed to getInputQuery to avoid hardcoding the query
+const getMetricsQuery = `
+  query{
+    getMetrics
+  }
+`;
 
-interface MetricNode {
-  metric: string;
-  measurements: Measurement[];
-}
-
-const getMetricNames = async () => {
-  const res = await client.query({
-    query: gql`
-      query {
-        getMetrics
-      }
-    `,
+//builds a query to be passed into getDataQuery
+const getInputQuery = (metrics: string[]) => {
+  return metrics.map(metric => {
+    return `{ metricName: "${metric}", after: ${thirtyMinutesAgo} }`;
   });
-  return res.data.getMetrics;
 };
 
 const getDataQuery = (inputQuery: string[]) => {
@@ -49,7 +40,7 @@ const getDataQuery = (inputQuery: string[]) => {
 `;
 };
 
-const measurementsSubscriptionQuery = gql`
+const newMeasurementsSub = gql`
   subscription {
     newMeasurement {
       metric
@@ -60,14 +51,16 @@ const measurementsSubscriptionQuery = gql`
   }
 `;
 
-const minInMilliSec = new Date(Date.now() - 60 * 60000).getTime();
-const getInputQuery = (metrics: string[]) => {
-  return metrics.map(metric => {
-    return `{ metricName: "${metric}", after: ${minInMilliSec} }`;
+const fetchMetrics = async () => {
+  const res = await client.query({
+    query: gql`
+      ${getMetricsQuery}
+    `,
   });
+  return res.data.getMetrics;
 };
 
-const getMulitpleMeasurementsData = async (metrics: string[]) => {
+const fetchData = async (metrics: string[]) => {
   const res = await client.query({
     query: gql`
       ${getDataQuery(getInputQuery(metrics))}
@@ -76,22 +69,52 @@ const getMulitpleMeasurementsData = async (metrics: string[]) => {
   return res.data.getMultipleMeasurements;
 };
 
+//values coming from the subscription are in this format, as well as being a key/value pair in MetricNode objects
+export interface Measurement {
+  metric: string;
+  at: number;
+  value: number;
+  unit: string;
+}
+
+//interface for the subscription
+interface MeasurementSub {
+  newMeasurement: Measurement;
+}
+
+//response from getDataQuery is an array of MetricNodes
+interface MetricNode {
+  metric: string;
+  measurements: Measurement[];
+}
+
+//filters the transformed data to only contain data pertaining to selected metrics
 const dataFilter = (data: Plotly.Data[], selection: (string | undefined)[]) => {
-  let filteredArr = data.filter(metricObj => {
+  console.log('mertirc: ', data, selection);
+  let returnArr = data.filter(metricObj => {
     return selection.includes(metricObj.name);
   });
-  filteredArr.push({
+
+  //workaround for limitation with Plotly - it was unable to display pressure and injValveOpen
+  //together without having temperature present. This dummy object tricks it into thinking the primary yaxis is present.
+  const dummyObj: Plotly.Data = {
     x: [],
     y: [],
     name: '',
     yaxis: 'y',
     type: 'scatter',
     line: { color: '#444' },
-  });
-  return filteredArr;
+  };
+
+  returnArr.push(dummyObj);
+
+  console.log('retur: ', returnArr);
+
+  return returnArr;
 };
 
-const transformToChartData = (data: MetricNode[]) => {
+//transforms the gql data object to a format compatible with Plot.ly
+const dataTransformer = (data: MetricNode[]) => {
   const returnArr: Plotly.Data[] = [];
   const colorArr: string[] = ['#a83a32', '#2d8fa1', '#5ba12d', '#9c2894', '#e6ad8e', '#32403f'];
   data.forEach(metricNode => {
@@ -123,75 +146,87 @@ const transformToChartData = (data: MetricNode[]) => {
   return returnArr;
 };
 
-const Dashboard = () => {
-  const [metricNames, setMetricNames] = React.useState<string[]>([]);
+export default () => {
+  const [metricStrings, setMetricStrings] = React.useState<string[]>([]);
   const [selection, setSelection] = React.useState<(string | undefined)[]>([]);
   const [initialData, setInitialData] = React.useState<Plotly.Data[]>([]);
   const [filteredData, setFilteredData] = React.useState<Plotly.Data[]>([]);
-  const { loading, data } = useSubscription<MeasurementSubscription>(measurementsSubscriptionQuery);
-  const [prevSubData, setPrevSubData] = React.useState<Measurement>({ metric: '', at: 0, value: 0, unit: '' });
-  const [latestData, setLatestData] = React.useState<Measurement[]>([]);
+  const { loading, data } = useSubscription<MeasurementSub>(newMeasurementsSub);
+  const [prevSubData, setPrevSubData] = React.useState<Measurement>({metric: "", at: 0, value: 0, unit: ""});
+  const [latestData, setLatestData] = React.useState<Measurement[]>([])
 
+  //initial "run" logic
   React.useEffect(() => {
     const initialFetch = async () => {
-      const metricsNamesList = await getMetricNames();
-      setMetricNames(metricsNamesList);
+      //grabs metric names to avoid hardcoding
+      const metricsRes = await fetchMetrics();
 
-      const responseData = await getMulitpleMeasurementsData(metricsNamesList);
-      const transformedData = transformToChartData(responseData);
+      //fetches data based on metrics present in the API
+      const dataRes = await fetchData(metricsRes);
+
+      //transform the data to a format compatible with Plot.ly
+      const transformedData = dataTransformer(dataRes);
+
+      //set metrics to populate select menu
+      setMetricStrings(metricsRes);
+
+      //dynamically create a template based on metrics in API for subscription data to be pushed into 
+      //to be displayed on taskbar
+      let initialLatestData: Measurement[] = [] 
+      metricsRes.forEach((metric: string)=>{
+        initialLatestData.push({metric: metric, at: 0, value: 0, unit: ""})
+      })
+      setLatestData(initialLatestData);
+
+      //set the master data object, and trigger the useEffect on line 194
       setInitialData(transformedData);
-
-      let tempData: Measurement[] = [];
-      metricsNamesList.forEach((metric: string) => {
-        tempData.push({ metric: metric, at: 0, value: 0, unit: '' });
-      });
-      setLatestData(tempData);
     };
     initialFetch();
   }, []);
 
   React.useEffect(() => {
+    //upon initial load, and when the menu selection changes, filter the data so that only selected data is rendered
     const filteredDataValue = dataFilter(initialData, selection);
     setFilteredData(filteredDataValue);
   }, [initialData, selection]);
 
-  React.useEffect(() => {
-    if (
-      !loading &&
-      data &&
-      (data.newMeasurement.at !== prevSubData.at ||
-        data.newMeasurement.value !== prevSubData.value ||
-        data.newMeasurement.metric !== prevSubData.metric)
-    ) {
-      let measurementNode = data.newMeasurement;
-      let matchingSet = initialData.find(metricNode => metricNode.name === measurementNode.metric);
-      if (matchingSet && measurementNode) {
-        (matchingSet.x as Plotly.Datum[]).push(new Date(measurementNode.at));
-        (matchingSet.y as Plotly.Datum[]).push(measurementNode.value);
-        const updatedData = initialData.map(metricNode => {
-          if (metricNode.name === measurementNode.metric) {
-            return matchingSet;
-          } else {
-            return metricNode;
-          }
-        });
-        setInitialData(updatedData as Plotly.Data[]);
-        if (data) {
-          let latestDataTemplate = latestData.map(measurement => {
-            return measurement.metric === data.newMeasurement.metric ? data.newMeasurement : measurement;
+  React.useEffect(()=>{
+    //check the latest emission from the subscription and evaluate if the data within is updated
+    if (!loading && data && (data.newMeasurement.at !== prevSubData.at || data.newMeasurement.value !== prevSubData.value || data.newMeasurement.metric !== prevSubData.metric)) {
+        let measurementNode = data.newMeasurement
+        let matchingSet = initialData.find((metricNode)=>metricNode.name === measurementNode.metric);
+        if (matchingSet && measurementNode){
+          //push the new data into the corresponding metric's data array
+          (matchingSet.x as Plotly.Datum[]).push(new Date(measurementNode.at));
+          (matchingSet.y as Plotly.Datum[]).push(measurementNode.value);
+          const updatedData = initialData.map((metricNode)=>{
+            if(metricNode.name === measurementNode.metric){
+              return matchingSet
+            } else {
+              return metricNode
+            }
           });
-          setLatestData(latestDataTemplate);
-          setPrevSubData(data.newMeasurement);
+          //refresh the data in state
+          setInitialData(updatedData as Plotly.Data[]);
+          if (data) {
+            //replace the corresponding measurement within the latestData state object
+            let latestDataTemplate = latestData.map((measurement)=>{
+              return measurement.metric === data.newMeasurement.metric ? data.newMeasurement : measurement
+            })
+            setLatestData(latestDataTemplate)
+
+            //save this measurement to check against new subscription emissions
+            setPrevSubData(data.newMeasurement)
+          }
         }
       }
-    }
-  }, [initialData, loading, data, prevSubData, latestData]);
+  },[initialData, loading, data, prevSubData, latestData])
 
   return (
     <Card style={{margin: 24}}>
       <Header/>
       <DashboardHeader
-        metrics={metricNames}
+        metrics={metricStrings}
         selection={selection}
         setSelection={setSelection}
         latestData={latestData}
@@ -202,5 +237,3 @@ const Dashboard = () => {
     </Card>
   );
 };
-
-export default Dashboard;
